bde5f9b9e1956a2f9e78302edca44f12ca241eef
Muhammad Moinuddin <moinuddinilyas786@gmail.com>
2023-05-13 00:05:24 +0500

    First Commit
    Completed UI part and completed navigation

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const app = new express();
+app.use(express.static('public'))
+
+app.listen(5000,() => {
+    console.log("Listen on 5000 port");
+})
+
+app.get('/',(req, res) => {
+    res.sendFile(path.resolve(__dirname,'pages/index.html'))
+})
+
+app.get('/contact',(req, res) => {
+    res.sendFile(path.resolve(__dirname,'pages/contact.html'))
+})
+
+app.get('/about',(req, res) => {
+    res.sendFile(path.resolve(__dirname,'pages/about.html'))
+})
+
+app.get('/post',(req, res) => {
+    res.sendFile(path.resolve(__dirname,'pages/post.html'))
+})
